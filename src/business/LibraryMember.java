1961c49836c4ce03bad3d0791c96514bff933394
Thuong Nguyen <mttbit@gmail.com>
2022-03-01 15:10:10 -0600

    Add models

---
+++ src/business/LibraryMember.java
@@ -0,0 +1,13 @@
+package business;
+
+public class LibraryMember {
+    private String memberId;
+
+    public LibraryMember(String memberId, String firstName, String lastName, String phone, Address address) {
+
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+}
