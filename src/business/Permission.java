b243ab7a9c373994b73852ebd76ddcfe8ade38cb
Thuong Nguyen <mttbit@gmail.com>
2022-03-03 15:38:12 -0600

    Update class diagram and permission relationship

--- src/business/Permission.java
+++ src/business/Permission.java
@@ -24,46 +24,9 @@ public class Permission {
     private final PermissionID id;
     private final String name;
 
-    private Permission(PermissionID id, String name) {
+    public Permission(PermissionID id, String name) {
         this.id = id;
         this.name = name;
-    }
-
-    /**
-     * Factory method to get permissions base on Authorizationn
-     * @param authorization
-     * @return List of permissions for authorization
-     */
-    public static List<Permission> permissionsWithAuth(Auth authorization) {
-        List<Permission> permissions = new ArrayList<>();
-        switch (authorization) {
-            case LIBRARIAN: {
-                permissions.add(new Permission(PermissionID.CheckoutBook, "Checkout a book for a library member"));
-                permissions.add(new Permission(PermissionID.PrintCheckoutRecord, "Print the checkout record of library member"));
-                break;
-            }
-
-            case ADMIN: {
-                permissions.add(new Permission(PermissionID.AddNewLibraryMember, "Add a new library member to the system."));
-                permissions.add(new Permission(PermissionID.AddCopyOfExistingBook, "Add a copy of an existing book to the library collection."));
-                permissions.add(new Permission(PermissionID.AddABook, "Add a book to the library collection."));
-                break;
-            }
-
-            case BOTH: {
-                permissions.add(new Permission(PermissionID.AddNewLibraryMember, "Add a new library member to the system."));
-                permissions.add(new Permission(PermissionID.CheckoutBook, "Checkout a book for a library member"));
-                permissions.add(new Permission(PermissionID.AddCopyOfExistingBook, "Add a copy of an existing book to the library collection."));
-                permissions.add(new Permission(PermissionID.AddABook, "Add a book to the library collection."));
-                permissions.add(new Permission(PermissionID.PrintCheckoutRecord, "Print the checkout record of library member"));
-                break;
-            }
-        }
-
-        // Default permission
-        permissions.add(new Permission(PermissionID.Logout, "Logout"));
-
-        return permissions;
     }
 
     public PermissionID getId() {
